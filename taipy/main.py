5b4f692eec99d91e37da019f1f5757700031397d
Isaac Jiang <isaac.jiang66@gmail.com>
2023-08-19 23:44:51 -0400

    analytics

--- taipy/main.py
+++ taipy/main.py
@@ -1,23 +1,101 @@
-from taipy import Gui
+from taipy.gui import Gui, navigate
 import pandas as pd
+import random
 
-path = None
+memory_path = None
+ext_path = None
+data = None
+
+integers_column = None
+areas = None
+color_dict = None
+data_dict = None
+options = {
+    # Hide the texts
+    "textinfo": "none"
+}
+
+column2 = None
+color_dict1 = None
+data_dict1 = None
+options1 = {
+    # Hide the texts
+    "textinfo": "none"
+}
 
 page_1 = """
-# Hello there!
+<center> <h1> Hello there! </h1> </center>
+<center> <|{memory_path}|file_selector|label=Upload Memory CSV|on_action=memory_action|extensions=.csv|> </center>
 
-<|{path}|file_selector|label=Upload dataset|on_action=load_csv_file|extensions=.csv|>
+<center> <|{ext_path}|file_selector|label=Upload Extensions CSV|on_action=ext_action|extensions=.csv|> </center>
 """
 
 page_2 = """
-# Folder Statistics
+<center> <h1> Memory Statistics </h1> </center>
+
+<|{data_dict}|chart|type=pie|values=Area|label=Extensions|layout={color_dict}|options={options}|>
 """
 
-def load_csv_file(state):
-    data = pd.read_csv(state.path)
-    print(data)
+page_3 = """
+<center> <h1> Extension Statistics </h1> </center>
+
+<|{data_dict1}|chart|type=pie|values=Area|label=Extensions|layout={color_dict1}|options={options}|>
+"""
+
+def memory_action(state):
+    global data, integers_column, areas, color_dict, data_dict
+
+    data = pd.read_csv(state.memory_path, sep="\t", header=None, names=["Extensions", "Column2", "Area"])
+
+    # Extract extensions from filenames
+    extensions_first_index = [extension.split(",")[0] for extension in data["Extensions"]]
+
+    # Get the unique extensions
+    unique_extensions = set(extensions_first_index)
+    integers_column = [extension.split(",")[1] for extension in data["Extensions"]]
+    integers_column = [int(value) for value in integers_column]
+    print(integers_column)
+
+    areas = [extension.split(",")[2] for extension in data["Extensions"]]
+    areas = [int(value) for value in areas]
+    print(areas)
+
+    # Generate random hex color codes for each unique extension
+    color_codes = [
+        "#" + "".join(random.choices("0123456789ABCDEF", k=6))
+        for _ in range(len(unique_extensions))
+    ]
+
+    # Create a dictionary in the desired format
+    state.color_dict = {"piecolorway": color_codes, "title": "Memory Usage By File", "showlegend": False}
+    # Create a dictionary in the desired format
+    state.data_dict = {"Extensions": extensions_first_index, "Area": areas}
+
+    navigate(state, "memory_stats")
+
+def ext_action(state):
+    global data, column2, color_dict1, data_dict1
+    data = pd.read_csv(state.ext_path, sep="\t", header=None, names=["Extensions", "Area"])
+
+    extensions_first_index1 = [extension.split(",")[0] for extension in data["Extensions"]]
+    unique_extensions = set(extensions_first_index1)
+    print(extensions_first_index1)
+
+    column2 = [extension.split(",")[1] for extension in data["Extensions"]]
+    column2 = [float(value) for value in column2]
+
+    color_codes = [
+        "#" + "".join(random.choices("0123456789ABCDEF", k=6))
+        for _ in range(len(unique_extensions))
+    ]
+
+    state.color_dict1 = {"piecolorway": color_codes, "title": "Extension Make-Up By Percentage", "showlegend": False}
+
+    state.data_dict1 = {"Extensions": extensions_first_index1, "Area": column2}
+
+    navigate(state, "ext_stats")
 
 if __name__ == "__main__":
-    pages = {"home": page_1}
+    pages = {"home": page_1, "memory_stats": page_2, "ext_stats": page_3}
     gui = Gui(pages=pages)
     gui.run(use_reloader=True, port=8080)
